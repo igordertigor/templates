7666497c29a1943e4f315b17dc93ef3acfab0eca
Ingo <ingo@verbally.de>
2022-11-09 17:06:32 +0100

    Fix hook for venv installation

--- explore-datascience/hooks/post_gen_project.sh
+++ explore-datascience/hooks/post_gen_project.sh
@@ -2,11 +2,11 @@
 
 set -xe
 
-{% if cookiecutter.git_init %}
+{% if cookiecutter.git_init.lower().startswith("y") %}
 git init
 {% endif %}
 
-{% if cookiecutter.create_venv %}
+{% if cookiecutter.create_venv.lower().startswith("y") %}
 python3 -m venv .venv/
 source .venv/bin/activate
 pip install -r requirements.txt
