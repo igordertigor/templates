686be49ffc0502a5919017ed3d83b68e7bf92d7e
Ingo <ingo@verbally.de>
2023-06-20 11:44:18 +0200

    cli

---
+++ command-line-tool/hooks/post_gen_project.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import os
+import re
+
+commandline_entrypoint = "{{ cookiecutter.commandline_entrypoint }}"
+package_name = "{{ cookiecutter.package_name }}".strip('/')
+
+if '=' in commandline_entrypoint:
+    command, commandline_entrypoint = commandline_entrypoint.split('=')
+else:
+    command = package_name
+command = command.strip()
+
+entry_script, entry_func = commandline_entrypoint.strip().split(':')
+
+if entry_script.startswith(package_name):
+    entry_script = entry_script[len(package_name)+1:]
+
+cleaned_entrypoint = f'{command} = {package_name}.{entry_script}:{entry_func}'
+
+if entry_script.startswith('.'):
+    entry_script = entry_script.replace('.', '/')
+
+if entry_script.startswith('/'):
+    entry_script = entry_script[1:]
+
+if len(entry_script) == 0:
+    fname = os.path.join('src', package_name, '__main__.py')
+else:
+    if not entry_script.endswith('.py'):
+        entry_script = entry_script + '.py'
+
+    fname = os.path.join('src', package_name, entry_script)
+
+with open(fname, 'w') as f:
+    f.write('\n'.join([
+        '#!/usr/bin/env python',
+        'import typer',
+        '',
+        'app = typer.Typer()',
+        '',
+        '@app.callback()',
+        'def main():',
+        '    # Set global options here',
+        '    pass',
+        '',
+        '',
+        '@app.command()',
+        'def example():',
+        '    pass',
+        '',
+        '',
+        'if __name__ == \'__main__\':',
+        '    app.run()'
+    ]))
+
+
+with open('setup.cfg') as f:
+    txt = f.read()
+
+with open('setup.cfg', 'w') as f:
+    f.write(re.sub(
+        r'__post_hook.commandline_entrypoint__',
+        cleaned_entrypoint,
+        txt,
+    ))
---
+++ command-line-tool/{{ cookiecutter.package_name }}/tests/cram/README.md
@@ -0,0 +1,1 @@
+[Cram tests](https://bitheap.org/cram/) can be used to test command line interfaces
